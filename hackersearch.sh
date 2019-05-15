842c5100f3111d2edb44a8051a6324f682a26019
mamachanko <sugardubz@gmail.com>
2019-05-15 11:28:29 +0200

    search hn by user query

--- hackersearch.sh
+++ hackersearch.sh
@@ -2,13 +2,21 @@
 
 set -euo pipefail
 
-echo "Press <space> to search Hackernews, <q> to quit."
+trap ctrl_c INT
+
+function ctrl_c() {
+    echo
+    echo ok. bye.
+    exit
+}
+
+echo "Search for the 5 latest Hackernews stories."
+echo "Type your query and press <enter>."
+echo
+
 while [ true ] ; do
-    read -s -n 1 key
-    if [[ $key = '' ]] ; then
-        curl -sL "https://hn.algolia.com/api/v1/search_by_date?tags=story&query=Bangalore" | jq --raw-output '.hits[0:5] | map(.title)' ;
-    elif [[ $key = q ]] ; then
-        echo ok. bye.
-        exit ;
-    fi
+    read -p ">_ " query
+    echo -ne " loading ...\r"
+    curl -sL "https://hn.algolia.com/api/v1/search_by_date?tags=story&query=${query}" | jq --raw-output '.hits[0:5] | map(.title) | .[]' ;
+    echo
 done
