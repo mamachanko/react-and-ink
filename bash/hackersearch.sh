1ba7981b4541768d7be224522df0f37efbb8de36
mamachanko <sugardubz@gmail.com>
2019-05-21 11:47:48 +0200

    refactor bash script to be more readable

--- bash/hackersearch.sh
+++ bash/hackersearch.sh
@@ -10,13 +10,20 @@ function ctrl_c() {
     exit
 }
 
-echo "Search for the 5 latest Hackernews stories."
+echo "Search for Hackernews stories by date."
 echo "Type your query and press <enter>."
 echo
 
 while [ true ] ; do
     read -p ">_ " query
     echo -ne " loading ...\r"
-    curl -sL "https://hn.algolia.com/api/v1/search_by_date?tags=story&query=${query}" | jq --raw-output '.hits[0:5] | map(.title) | .[]' ;
+    curl \
+        --get \
+        --url "https://hn.algolia.com/api/v1/search_by_date" \
+        --data-urlencode "tags=story" \
+        --data-urlencode "query=${query}" \
+        --silent \
+        --location \
+    | jq --raw-output '.hits[0:5] | map(.title) | .[]'
     echo
 done
